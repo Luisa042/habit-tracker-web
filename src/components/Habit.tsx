9b16b34554adb5a2f73d0a2f4196d91b9c005d25
Luisa <luisa.santos042@gmail.com>
2023-01-19 12:22:09 -0300

    some style changes

--- src/components/Habit.tsx
+++ src/components/Habit.tsx
@@ -1,14 +1,11 @@
-import './Habit.css';
-
 interface HabitProps {
   title: string;
 }
 
 export function Habit(props: HabitProps) {
   return (
-    <div className='habit'>
+    <div className='bg-zinc-900 w-10 h-10 text-white rounded m-2 text-center flex items-center justify-center'>
       <h3>{props.title}</h3>
-      <p>count</p>
     </div>
   );
 }
